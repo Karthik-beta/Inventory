36927a2b079ed53c43f2ae1253b711d857bd4162
Karthik-beta <karthikpar@protonmail.com>
2025-04-20 10:11:56 +0530

    0.0.1

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,15 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import viteCompression from 'vite-plugin-compression' // add this
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    viteCompression({ algorithm: 'brotliCompress' }), // add this
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -13,6 +18,25 @@ export default defineConfig({
       // fix loading all icon chunks in dev mode
       // https://github.com/tabler/tabler-icons/issues/1233
       '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
+  build: {
+    sourcemap: false, // disable sourcemaps in production
+    minify: 'esbuild', // use esbuild for faster minification
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react'
+            if (id.includes('@tanstack')) return 'vendor-tanstack'
+            return 'vendor'
+          }
+        },
+        // preload critical chunks
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
     },
   },
 })
