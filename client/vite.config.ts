81aefc5435697db337e07ed6dcf4485f2c38e801
Karthik-beta <karthikpar@protonmail.com>
2025-04-20 13:10:47 +0530

    0.0.1

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,33 +2,31 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import viteCompression from 'vite-plugin-compression' // add this
+import viteCompression from 'vite-plugin-compression'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     TanStackRouterVite(),
-    viteCompression({ algorithm: 'brotliCompress' }), // add this
+    // Apply compression only in production
+    viteCompression({ algorithm: 'brotliCompress', disable: process.env.NODE_ENV !== 'production' }),
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-
-      // fix loading all icon chunks in dev mode
-      // https://github.com/tabler/tabler-icons/issues/1233
       '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
     },
   },
   build: {
-    sourcemap: false, // disable sourcemaps in production
-    minify: 'esbuild', // use esbuild for faster minification
+    sourcemap: false,
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
             if (id.includes('react')) return 'vendor-react'
             if (id.includes('@tanstack')) return 'vendor-tanstack'
+            // Group other dependencies into a single vendor chunk
             return 'vendor'
           }
         },
